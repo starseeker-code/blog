e589e58f183757a1bfc81ec1fba58ab7aa300f47
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-02-12 17:21:25 +0100

    Basic models created for posts and added app to settings

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=245)
+    slug = models.SlugField(max_length=100)
+    body = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)  # Cuando se publico el post en la base de datos
+    creado = models.DateTimeField(auto_now_add=True)  # Cuando se creo el post
+    actualizado = models.DateTimeField(auto_now=True)  # Ultimo cambio en el post
+    
+    def __str__(self):
+        return f"Post: {self.slug}"
+    
+    class Meta:
+        ordering = ["-publicado"]  # Muestra los posts de acuerdo a cuando se publicaron
+        indexes = [
+            models.Index(fields=["-publicado"])  # Indexa los posts
+        ]
