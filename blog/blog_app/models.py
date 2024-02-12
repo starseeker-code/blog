d97a2e38ef6dc4c1736634f2061d6d27c09165dc
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-02-12 19:01:54 +0100

    Created basic views and finished models for posts

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
+    
+    class Status(models.TextChoices):  # Status para un post
+        BORRADOR = "DF", "Draft"
+        PUBLICADO = "PB", "Published"
+    
     titulo = models.CharField(max_length=245)
     slug = models.SlugField(max_length=100)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")  # Relacion N:1 con User (importado de auth.models)
     body = models.TextField()
     publicado = models.DateTimeField(default=timezone.now)  # Cuando se publico el post en la base de datos
     creado = models.DateTimeField(auto_now_add=True)  # Cuando se creo el post
     actualizado = models.DateTimeField(auto_now=True)  # Ultimo cambio en el post
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.BORRADOR)  # Uso de la clase TextChoices (Status)
     
     def __str__(self):
         return f"Post: {self.slug}"
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.publicado.all()  # Lista todos los posts
+    return render(request, "blog/post/all_posts.html", {"posts": posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post, id=id,  # Extrae por ID
+        status=Post.Status.PUBLICADO  # Solo los posts publicados
+    )
+    return render(request, "blog/post/post_detail.html", {"post": post})
